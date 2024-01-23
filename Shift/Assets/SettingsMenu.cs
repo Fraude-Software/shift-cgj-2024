e26bb18e1f49498db9bc0f78a0d7f11264518df8
mickael <mickaelcoulon34570@gmail.com>
2024-01-23 21:36:03 +0100

    mainmenu

---
+++ Shift/Assets/SettingsMenu.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    Resolution[] resolutions;
+
+    public TMPro.TMP_Dropdown resolutionDropDown;
+
+    public void Start(){
+        resolutions = Screen.resolutions;
+        resolutionDropDown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+
+        for (int i=0; i < resolutions.Length;i++){
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.width && resolutions[i].height == Screen.height){
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropDown.AddOptions(options);
+        resolutionDropDown.value = currentResolutionIndex;
+        resolutionDropDown.RefreshShownValue();
+
+        Screen.fullScreen = true;
+    }
+
+    public void SetVolume(float volume){
+        audioMixer.SetFloat("Volume",volume);
+    }
+
+    public void SetFullScreen(bool isFullScreen){
+        Screen.fullScreen = isFullScreen;
+    }
+
+    public void SetResolution(int resolutionIndex){
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+}
